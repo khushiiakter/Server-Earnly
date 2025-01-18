c95a0271627f5fc647d1b4ce68b1734c216d248c
khushiiakter <rkjia1311@gmail.com>
2025-01-18 15:17:35 +0600

    added role and coin value when first register

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.7xkdi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -20,7 +17,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -31,48 +28,65 @@ async function run() {
     const tasksCollection = client.db("earnlyDb").collection("tasks");
     const usersCollection = client.db("earnlyDb").collection("users");
 
-    
+    // app.post('/users/:email', async (req, res) => {
 
-    app.post('/users/:email', async (req, res) => {
-      
-      const { name, email, image, role }  = req.body;
-      
-      const initialCoins = role === 'Worker' ? 10 : 50;
-      const user = {
-        name,
-        email,
-        image,
-        role: role || "Worker",
-        coins: initialCoins,
-        timestamp: new Date(),
-      };
+    //   const { name, email, image, role }  = req.body;
+
+    //   const initialCoins = role === 'Worker' ? 10 : 50;
+    //   const user = {
+    //     name,
+    //     email,
+    //     image,
+    //     role: role || "Worker",
+    //     coins: initialCoins,
+    //     timestamp: new Date(),
+    //   };
+
+    //   const filter = { email };
+    //   const options = { upsert: true };
+    //   const updateDoc = {
+    //     $setOnInsert: user,
+    //   };
+
+    //   const result = await usersCollection.updateOne(filter, updateDoc, options);
+    //   res.send(result);
+    // });
+
+    app.post("/users/:email", async (req, res) => {
+      const { name, email, image, role } = req.body;
 
       const filter = { email };
-      const options = { upsert: true };
       const updateDoc = {
-        $setOnInsert: user, 
+        $set: {
+          name: name || "Anonymous",
+          image: image || "",
+          role: role || "Worker",
+          timestamp: new Date(),
+        },
+        $setOnInsert: {
+          coins: role === "Worker" ? 10 : 50, 
+        },
       };
-    
-      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const options = { upsert: true };
+
+      const result = await usersCollection.updateOne(
+        filter,
+        updateDoc,
+        options
+      );
       res.send(result);
     });
 
-
-    
-
-
-
-
-    app.get('/tasks', async(req, res) => {
-        const result = await tasksCollection.find().toArray();
-        res.send(result);
-    })
-
-
+    app.get("/tasks", async (req, res) => {
+      const result = await tasksCollection.find().toArray();
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -80,13 +94,10 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("Earnly is running");
+});
 
-
-
-app.get('/', (req, res) => {
-    res.send('Earnly is running')
-  })
-  
-  app.listen(port, () => {
-    console.log(`Earnly is sitting on port ${port}`);
-  })
+app.listen(port, () => {
+  console.log(`Earnly is sitting on port ${port}`);
+});
